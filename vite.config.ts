6ae2a50de7c97d0d0c16906440384afab5e53d0c
chuckle <1214182200@qq.com>
2023-04-06 20:16:00 +0800

    修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    base: '/blog' 
+  },
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'docs'
   }
 })
