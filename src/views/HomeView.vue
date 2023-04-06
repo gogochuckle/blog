450c18ab48225d4039bae453032baf04852304b1
chuckle <1214182200@qq.com>
2023-04-06 19:41:28 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <header>
+      <a href="" class="a-logo" >
+        <img alt="Vue logo" class="logo" src="../assets/logo.svg" />
+      </a>
+      <div class="motto" >
+        <a class="motto-a" href="">永远追求</a>，
+        <a class="motto-a" href="">永远尝试</a>。
+      </div>
+    </header>
+  </main>
+</template>
+
+<style scoped>
+
+header {
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+}
+
+.a-logo {
+  user-select: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+}
+
+.logo {
+  width: 55rem;
+  height: 10rem;
+  margin: 0;
+  user-select: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+}
+
+.motto {
+  color: #000;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 20px;
+  white-space: normal;
+  user-select: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+}
+
+motto-a {
+  color: #000;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+a {
+  text-decoration: none
+}
+</style>
